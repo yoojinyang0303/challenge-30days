c7290a5a0a84f66bcfaf75a84838b25501d35846
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-10-17 02:05:57 +0900

    Chore: 개발환경 setup

---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyB-SoxNW_ZlRoRmMSvExI58qEcEEXVbJ6Y",
+  authDomain: "challenge-30days.firebaseapp.com",
+  projectId: "challenge-30days",
+  storageBucket: "challenge-30days.appspot.com",
+  messagingSenderId: "450610343479",
+  appId: "1:450610343479:web:1e129e01e4fbb6203a19b2",
+};
+
+const app = initializeApp(firebaseConfig);
