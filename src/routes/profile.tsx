7eb4fedf69b0f0b84d5c04435fc32633e2760d31
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-10-24 21:34:28 +0900

    Feat: 마이페이지 - 프로필 이미지 업로드 및 변경 구현

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,79 @@
+import { useState } from "react";
+import { auth, storage, updateProfile } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { styled } from "styled-components";
+import * as React from "react";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  overflow: hidden;
+  border-radius: 50%;
+  cursor: pointer;
+  background-color: #605f5e;
+  svg {
+    width: 50px;
+  }
+`;
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+
 export default function Profile() {
-  return <h1>Profile!</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {Boolean(avatar) ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        id="avatar"
+        type="file"
+        accept="image/*"
+        onChange={onAvatarChange}
+      />
+      <Name>{user?.displayName ?? "Anonymous"} </Name>
+    </Wrapper>
+  );
 }
